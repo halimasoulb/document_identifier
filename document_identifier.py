8054fab339a6cbd0f84a6928dd90c53276ebb3a9
halima <soulb159@gmail.com>
2020-08-05 15:35:01 +0100

    exp_reg

--- document_identifier.py
+++ document_identifier.py
@@ -168,6 +168,9 @@ def main():
             
             text=text_detector.text(boxes,texts)
 
+            result1=re.search(r" [0-9]{2}[ ,1]?[0-9]{2}[ ,1]?[0-9]{4}", text).group()
+            print(re.sub(r"([0-9]{2}[ ,1]?)([0-9]{2}[ ,1]?)([0-9]{4})", r"date de naissance: \1-\2-\3 ", result1))
+
             result=re.search(r"[0-9]{8}", text).group()
             date_de_validite=datetime.datetime.strptime(result, '%d%m%Y')
             print(' Date de validite:', date_de_validite.date())
